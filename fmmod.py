aa6618082ac49d9141d699495fc705487e6cbc73
David Fraser <fraserd81@gmail.com>
2017-02-05 09:15:42 -0600

    Finish I2C Module + Added Support Of Seek Buttons and Connect + Added Time Display

---
+++ fmmod.py
@@ -0,0 +1,184 @@
+import RPi.GPIO as GPIO
+import smbus
+from time import sleep
+
+class data():
+    byte= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    ### Create A Data Holder For Registers 32 bytes in Decimal Form
+    ### Use HEX() to get hex values see below in display_reg() Function
+    ###
+    status=[0,0,0,87.5,0]	
+	    # Connected 
+	      # Stero / Mono
+                # RDS Available
+                  # Current Frequency
+	           # Extra2
+
+reset = 4	## Pin BCM 7
+adr = 0x10	## i2c Address Of The Radio Module
+
+reg = data()
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(reset, GPIO.OUT)
+
+bus = smbus.SMBus(1)	# Instanciate The SMBUS 
+
+def init_i2c():
+    GPIO.output(reset, GPIO.LOW)
+    sleep(.1)
+    GPIO.output(reset, GPIO.HIGH)
+    sleep(.1)
+    print"Connected"
+    reg.status[1] = 1
+
+
+def read_reg():
+    regdata = bus.read_i2c_block_data(adr,reg.byte[16],32)
+    print "Registers Updated"
+    for i in range(32):
+        reg.byte[i] = (regdata[i])	
+
+def write_reg():
+    tx_list = [reg.byte[17],			# 0X20
+               reg.byte[18],reg.byte[19],	# 0X30
+               reg.byte[20],reg.byte[21],	# 0X40
+               reg.byte[22],reg.byte[23],	# 0X50
+               reg.byte[24],reg.byte[25],	# 0X60
+               reg.byte[26],reg.byte[27],]	# 0X70
+              # reg.byte[28],reg.byte[29],	# 0X80
+              # reg.byte[30],reg.byte[31],]	# 0X90
+    ## Above Creates A List From The Data Class Of Decimal Values to Be Sent Out
+    ## With i2c_block_write. Offset by one due to command writing to first byte of 
+    ## 0x02 register ** No idea why it does this might be smbus mod or the chip
+    bus.write_i2c_block_data(adr,reg.byte[16],tx_list)
+    sleep(.5)	# ** Required Delay Otherwise Radio Doesn't Start
+
+def setup_osc():
+    reg.byte[26] = 0x81
+
+def volume():
+    reg.byte[23] = reg.byte[23] & 0xF0
+    reg.byte[23] = reg.byte[23] | 0x15
+
+
+def tune(station):
+    reg.station = station
+    newtune = station*10-875
+ #   print hex(int(newtune))
+    #reg.byte[18] = 0x00			## Clear The Tune Bit
+    reg.byte[19] = int(newtune)		## Addin New Frequency
+    write_reg()				## Write To Register
+    reg.byte[18] = 0x80			## Set Tune Bit High
+    write_reg()				## Write To Register
+    #reg.byte[18] = 0x00 		## 0x00 = 87.5 // 0xCD = 108.0
+    #write_reg()
+
+def enable_radio():
+    reg.byte[16] = 0x40
+    reg.byte[17] = 0x01
+
+def enable_spacing():
+    reg.byte[20] = 0x10
+    reg.byte[21] = 0x00
+    reg.byte[23] |= (1<<4)
+#    print(reg.byte[20] + reg.byte[21] + reg.byte[23])
+
+def seek_up():
+    # Seek UP
+    # Register 0x02   Allow Wrap/Set Direction/Start Seek
+    # Register 0x06   Set Min Seek Threshold/Setmin IMP threshold
+    read_reg()
+    reg.byte[16] = 0x46		#Clear Seek Bit
+    reg.byte[18] = 0x00		#Clear Tune/Seek STC
+    write_reg()
+    reg.byte[16] = 0x47		# Run Seek
+    write_reg()
+    read_reg()
+
+def seek_dn():
+    # Seek UP
+    # Register 0x02   Allow Wrap/Set Direction/Start Seek
+    # Register 0x06   Set Min Seek Threshold/Setmin IMP threshold
+    read_reg()
+    reg.byte[16] = 0x44         #Clear Seek Bit
+    reg.byte[18] = 0x00         #Clear Tune/Seek STC
+    write_reg()
+    reg.byte[16] = 0x45         # Run Seek
+    write_reg()
+    read_reg()
+
+def status():				#Experimental [ ]
+    read_reg()
+    if ((reg.byte[0] & 0x01) == 1): 
+        #print "Stereo"
+        reg.status[2] = "STEREO"
+    elif ((reg.byte[0] & 0x01 == 0)):
+        #print "Mono"
+        reg.status[2] = "MONO"
+	
+    frequency = reg.byte[3]
+    reg.status[4] = .1*frequency+87.5 	# Using Reg 0x0B (LSB)
+					# Decode Currrent Frequency
+#    print("Freq = " + str(reg.status[4]))
+
+
+
+def display_reg():
+    #read_reg()
+    print("0x00 = " + hex(reg.byte[12]) +" "+ hex(reg.byte[13]) + " CHIP")
+    print("0x01 = " + hex(reg.byte[14]) +" "+ hex(reg.byte[15]) + " FIRMWARE")
+    print("0x02 = " + hex(reg.byte[16]) +" "+ hex(reg.byte[17]))
+    print("0x03 = " + hex(reg.byte[18]) +" "+ hex(reg.byte[19]))
+    print("0x04 = " + hex(reg.byte[20]) +" "+ hex(reg.byte[21]))
+    print("0x05 = " + hex(reg.byte[22]) +" "+ hex(reg.byte[23]))
+    print("0x06 = " + hex(reg.byte[24]) +" "+ hex(reg.byte[25]))
+    print("0x07 = " + hex(reg.byte[26]) +" "+ hex(reg.byte[27]))
+    print("0x08 = " + hex(reg.byte[28]) +" "+ hex(reg.byte[29]))
+    print("0x09 = " + hex(reg.byte[30]) +" "+ hex(reg.byte[31]))
+    print("0x0A = " + hex(reg.byte[0])  +" "+ hex(reg.byte[1]))
+    print("0x0B = " + hex(reg.byte[2])  +" "+ hex(reg.byte[3]))
+    print("0x0C = " + hex(reg.byte[4])  +" "+ hex(reg.byte[5]))
+    print("0x0D = " + hex(reg.byte[6])  +" "+ hex(reg.byte[7]))
+    print("0x0E = " + hex(reg.byte[8])  +" "+ hex(reg.byte[9]))
+    print("0x0F = " + hex(reg.byte[10]) +" "+ hex(reg.byte[11]))
+
+    ### 	Hex Handling	####
+    #
+    #   test = int(reg.byte[12],16)<<8
+    #   hex(tes)   
+    #   Output '0x1200'
+    ###
+
+    #
+
+    #
+    #
+
+def start():
+    	#	Sequence:	1) Enable OSC
+	#			2) delay(500)
+	#			3) Enable Radio (REG 0x02 = 0x4001
+	#			4) Enable RDS & Set Config REG 0x04
+	#			5) Set Spacing 100 KHZ	(REG 0x05) 
+    init_i2c()
+    read_reg()
+    setup_osc()
+    write_reg()
+    read_reg()
+    enable_radio()
+    write_reg()
+    read_reg()
+    enable_spacing()
+    write_reg()
+    read_reg()
+    volume()
+    write_reg()
+    read_reg()
+    tune(94.5)
+
+
+    #read_reg()
+
--- main.py
+++ main.py
@@ -9,80 +9,43 @@ Config.set('graphics', 'width', '800')		#	Official Screen Size
 Config.set('graphics', 'height', '480')		#	800 x 480
 
 import os
+import time
 
-butttime = "69"
-TEMP = "38"
-MPH = "88"
-RPM = "420"
-INTAKETEMP = "0"
+
 
 class FmRadio(Widget):
-     
-	
+    import fmmod as rad
+    rad.start()	
+    def seekUP(self):
+        self.rad.seek_up()
 
-    #print(dir(connection))
-
-    def new_mph(r):
-        global MPH          #MPH
-        MPH = round((r.value.magnitude*0.621371))
-
-
-    def new_rpm(r):
-        global RPM          #RPM
-        RPM = r.value.magnitude
-  
-    def new_temp(r):
-        global TEMP         #Coolant Temperature
-        TEMP = r.value.magnitude
-
-    def new_intaketemp(r):
-        global INTAKETEMP         #Intake Temperature
-        INTAKETEMP = r.value.magnitude
-
-
-
-    def update(self,dt):
-        pass
+    def seekDN(self):
+        self.rad.seek_dn()
 
     def exit(self, btn):
         print("Exiting")
         quit(1)
 
     def connect(self, fuck):
-        	
-	if self.connection.is_connected() == True :
-	    print "connected\n"
-	    self.ids.but_Connect.background_color = 0,1,0,1
+	self.rad.start()        	
 
-	if self.connection.is_connected() == False :
-	    print "not connected\n"
-	    self.ids.but_Connect.background_color = 1,0,0,1
-    #pass
+    def update_freq(self):
+        self.ids.label_frequency.text = str(self.rad.reg.status[4])
 
-    def update(self, DT):
-	# DT Variable is time called from ClockShed Task
-		global butttime
-		global RPM
-        #self.ids.but_G1.text = str(INTAKETEMP) + "\ndegC"
-		#self.ids.but_RPM.text = str(RPM) + "\nRPM"
-        #self.ids.but_MPH.text = str(MPH) + "\nMPH"
-        #self.ids.but_TEMP.text = " " + str(TEMP) + "\ndegC" 
+    def update(self, fuck):
+        self.rad.status()
+        self.update_freq()
 
-
-#    connection.watch(obd.commands.COOLANT_TEMP, callback=new_temp)
-#    connection.watch(obd.commands.RPM, callback=new_rpm)
-#    connection.watch(obd.commands.SPEED, callback=new_mph)
-#    connection.watch(obd.commands.INTAKE_TEMP, callback=new_intaketemp)
-
-#    connection.start()
-	
+    def update_time(self, DT):
+        self.ids.label_UTC.text = time.asctime()
 
 class FmRadioApp(App):
     def build(self):
 	mainscreen = FmRadio()
 	# mainscreen.functions()
 	#connection = obd.OBD()
-        Clock.schedule_interval(mainscreen.update, 1.0/60.0)
+        Clock.schedule_interval(mainscreen.update, 1)
+        Clock.schedule_interval(mainscreen.update_time, 1)
 
         return mainscreen
 
